9218217ba9eab70c2f8417503b61481bec19260f
pkrodrigues <pkjrodrigues@gmail.com>
2023-01-27 09:32:22 -0300

    feat: addition of Consulta class

---
+++ src/main/java/com/lpiv/clinic/model/entity/Consulta.java
@@ -0,0 +1,35 @@
+package com.lpiv.clinic.model.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Consulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idConsulta;
+
+    @ManyToOne
+    private Medico medico;
+    @ManyToOne
+    private Paciente paciente;
+
+    private String procedimento;
+    private Date dataConsulta;
+    private Date horario;
+    private String Observacao;
+    private String convenio;
+    private double valor;
+    private double valorTotal;
+}
